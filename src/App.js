e2a1c7982491186e8f355d5ae72bc3116ef060f0
Joshua <jykim92@gmail.com>
2021-03-19 12:44:45 -0400

    components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Container, Grid } from '@material-ui/core'
 
+import Profile from './components/Profile';
+import Header from './components/Header';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+
+
 function App() {
   return (
     <div className="App">
       <Container>
         <Grid container>
           <Grid item xs={12} md={4} lg={3} style={{backgroundColor: 'blue'}}>
-            left
+            <Profile />
           </Grid>
           <Grid item xs style={{backgroundColor: 'red'}}>
-            right
+            <Header />
+            <Portfolio />
+            <Resume />
+            <Footer />
           </Grid>
         </Grid>
       </Container>
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      Hi from Header
+    </div>
+  )
+}
+
+export default Header
