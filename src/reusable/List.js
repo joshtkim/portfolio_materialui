3b46bdd80427c724eddef3b27293a55092968e65
Joshua <jykim92@gmail.com>
2021-03-24 18:34:04 -0400

    resuable list

---
+++ src/reusable/List.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const List = () => {
+  return (
+    <List>
+      <ListItem>
+        <ListItemText />
+      </ListItem>
+      <ListItem>
+        <ListItemText />
+      </ListItem>
+    </List>
+  )
+}
+
+export default List
